c7bcb17fdd20a05faa24aea0f458be16ee506a55
Duy Trung <duytrung.tcu@gmail.com>
2020-08-12 14:20:20 +0700

    tune dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -18,11 +18,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 const tailwindcss = require('tailwindcss')('./tailwind.config.js');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const simpleVars = require('postcss-simple-vars')({ silent: true });
 const precss = require('precss');
 
 // const plugins = [tailwindcss, autoprefixer, cssnano, purgecss];
 const corePlugins = [precss, tailwindcss];
+
 const plugins = isProduction ?
   [...corePlugins, autoprefixer, cssnano, purgecss] : corePlugins;
 
